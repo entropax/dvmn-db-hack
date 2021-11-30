f8acea6c4513ca52a2159f44378f07a6dc1ff3f8
Cain Entropax Maxwell <cainmax@posteo.mx>
2021-11-30 21:00:45 +0300

    Fix __main__ rule

--- script.py
+++ script.py
@@ -1,46 +1,22 @@
 from random import randint, choice
+
+from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 from datacenter.models import (
-    Schoolkid, Mark, Chastisement, Lesson, Commendation
+    Schoolkid,
+    Mark,
+    Chastisement,
+    Lesson,
+    Commendation
 )
 
 
-PRAISES = [
-    'Молодец!',
-    'Отлично!',
-    'Хорошо!',
-    'Гораздо лучше, чем я ожидал!',
-    'Ты меня приятно удивил!',
-    'Великолепно!',
-    'Прекрасно!',
-    'Ты меня очень обрадовал!',
-    'Именно этого я давно ждал от тебя!',
-    'Сказано здорово – просто и ясно!',
-    'Ты, как всегда, точен!',
-    'Очень хороший ответ!',
-    'Талантливо!',
-    'Ты сегодня прыгнул выше головы!',
-    'Я поражен!',
-    'Уже существенно лучше!',
-    'Потрясающе!',
-    'Замечательно!',
-    'Прекрасное начало!',
-    'Так держать!',
-    'Ты на верном пути!',
-    'Здорово!',
-    'Это как раз то, что нужно!',
-    'Я тобой горжусь!',
-    'С каждым разом у тебя получается всё лучше!',
-    'Мы с тобой не зря поработали!',
-    'Я вижу, как ты стараешься!',
-    'Ты растешь над собой!',
-    'Ты многое сделал, я это вижу!',
-    'Теперь у тебя точно все получится!',
-]
-
-
-def get_schoolkid(name_pattern='Фролов Иван'):
-    kid = Schoolkid.objects.filter(full_name__contains='Фролов Иван').get()
-    return kid
+def find_schoolkid(name_pattern):
+    try:
+        return Schoolkid.objects.get(full_name__contains=name_pattern)
+    except Schoolkid.MultipleObjectsReturned:
+        print('Нашлось больше одного ученика, уточните запрос')
+    except Schoolkid.DoesNotExist:
+        print('Такой ученик не найден, уточните запрос')
 
 
 def fix_marks(schoolkid):
@@ -54,16 +30,55 @@ def remove_chastisements(schoolkid):
     Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def commendation(schoolkid):
-    last_lesson = Lesson.objects.filter(
+def add_random_commendation(schoolkid, subject_title):
+    PRAISES = [
+        'Молодец!',
+        'Отлично!',
+        'Хорошо!',
+        'Гораздо лучше, чем я ожидал!',
+        'Ты меня приятно удивил!',
+        'Великолепно!',
+        'Прекрасно!',
+        'Ты меня очень обрадовал!',
+        'Именно этого я давно ждал от тебя!',
+        'Сказано здорово – просто и ясно!',
+        'Ты, как всегда, точен!',
+        'Очень хороший ответ!',
+        'Талантливо!',
+        'Ты сегодня прыгнул выше головы!',
+        'Я поражен!',
+        'Уже существенно лучше!',
+        'Потрясающе!',
+        'Замечательно!',
+        'Прекрасное начало!',
+        'Так держать!',
+        'Ты на верном пути!',
+        'Здорово!',
+        'Это как раз то, что нужно!',
+        'Я тобой горжусь!',
+        'С каждым разом у тебя получается всё лучше!',
+        'Мы с тобой не зря поработали!',
+        'Я вижу, как ты стараешься!',
+        'Ты растешь над собой!',
+        'Ты многое сделал, я это вижу!',
+        'Теперь у тебя точно все получится!',
+    ]
+    lessons = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
         group_letter=schoolkid.group_letter,
-        subject__title='Музыка'
-    ).order_by('-date')[0]
+        subject__title=subject_title,
+    ).order_by('?')
+    if not lessons:
+        return print('Такого предмета нет, повторите запрос')
+    lesson = lessons.first()
     Commendation.objects.create(
-        schoolkid=schoolkid,
-        created=last_lesson.date,
-        teacher=last_lesson.teacher,
-        subject=last_lesson.subject,
-        text=choice(PRAISES),
-    )
+            text=choice(PRAISES),
+            created=lesson.date,
+            schoolkid=schoolkid,
+            subject=lesson.subject,
+            teacher=lesson.teacher,
+        )
+
+
+if __name__ == '__main__':
+    print('Не запускайте скрипт напрямую, импортируйте необходимые функции')
